2201ed36815257c6554c77aa7a43cd8d90e98336
adaptide <as1kenjebaev@gmail.com>
2025-05-15 17:20:07 +0500

    fix: last version

---
+++ server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-3VDUVVMA.mjs";import"./chunk-4OAYQTB2.mjs";import"./chunk-R6AWMJCD.mjs";import"./chunk-PKU2TZZC.mjs";import"./chunk-L2PLJDO5.mjs";import"./chunk-DCFYTP2A.mjs";export{a as default};
---
+++ server/render-utils.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{g as o,h as r,i as n}from"./chunk-4OAYQTB2.mjs";import{pb as e}from"./chunk-L2PLJDO5.mjs";import"./chunk-DCFYTP2A.mjs";export{n as renderApplication,r as renderModule,e as \u0275Console,o as \u0275SERVER_CONTEXT};
