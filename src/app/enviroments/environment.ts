657b273328fc3179712bae8eef173861f750e0eb
assylzhan <assylzhvn@gmail.com>
2025-05-25 02:17:05 +0500

    fix: something design remaked

--- src/app/enviroments/environment.ts
+++ src/app/enviroments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: "https://api.openvoice.init.red/api/v1"
+  apiUrl: "https://admin.openvoice.iict.kz/api/v1"
 };
--- src/app/static-pages/about/about.component.ts
+++ src/app/static-pages/about/about.component.ts
@@ -1,17 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {NgForOf} from "@angular/common";
 import {FooterComponent} from "../../common-blocks/footer/footer.component";
+import {TranslatePipe, TranslateService} from "@ngx-translate/core";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-about',
   standalone: true,
   imports: [
     NgForOf,
+    TranslatePipe,
+    RouterLink,
     FooterComponent
   ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit{
+  currentLanguage: any;
+
+  constructor(private translateService: TranslateService) {
+  }
+
+  ngOnInit() {
+    this.currentLanguage = this.translateService.currentLang;
+  }
 
 }
